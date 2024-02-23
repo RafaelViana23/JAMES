0d09abc8b8672a2d725839dc5d8f5a742d4e1a8c
Rafael Viana <144288983+RafaelViana23@users.noreply.github.com>
2024-02-23 19:12:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ function speakThis(message) {
         speech.text = finalText;
     }
 
-    else if (message.includes('código pai')) {
+    else if (message.includes('chefe')) {
         const finalText = "Acesso permitido, bem vindo senhor Paulo, agora você tem acesso total ao sistema";
         speech.text = finalText;
     }
