2c26cc000b74ac93a2ceed7fbc2c2cd7e6b3857b
Rafael Viana <144288983+RafaelViana23@users.noreply.github.com>
2024-02-23 18:14:03 -0300

    Update app.js

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ function speakThis(message) {
         speech.text = finalText;
     } 
 
-        if (message.includes('derrick') || message.includes('base')) {
+        if (message.includes('derrick') || message.includes('derek') || message.includes('base')) {
         const finalText = "acesso permitido, bem vindo senhor Filipe, agora você tem acesso total ao sistema";
         speech.text = finalText;
     }
